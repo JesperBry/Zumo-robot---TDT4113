00e202ddecee95e96a6d9f977c287eca35d34bb1
Jesper Brynildsen <jesper_bry@hotmail.com>
2017-11-06 11:48:18 +0100

    Added Motob.py

---
+++ Motob.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Motob:
+
+    def __init__(sefl):
+        self.motors = []
+        self.value = int
+
+    def update(self):
+        # Receive a new motor recommendation, load it into the value slot, and operationalize it.
+
+    def operationalize(self):
+        # Convert a motor recommendation into one or more motor settings, which are sent to the corresponding motor(s).
