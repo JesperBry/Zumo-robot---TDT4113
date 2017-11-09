7c45afbb66614274e029eb57f696ef6d9b1450ec
mariussd <marius_dreyer@hotmail.com>
2017-11-09 11:38:35 +0100

    test print jesper

--- Arbitrator.py
+++ Arbitrator.py
@@ -2,6 +2,7 @@
 class Arbitrator:
     def __init__(self):
         self.behaviors = []
+        print("Jesper er deilig")
 
     def choose_behavior(self):
         #Skal velge en "vinner behavior" og sende dens motor-recommendation til BBCON
