39d1b61c35feba8c484c402655a6fdc25f4fdbd4
Jesper Brynildsen <jesper_bry@hotmail.com>
2017-11-09 12:42:42 +0100

    added choose_action

--- Arbitrator.py
+++ Arbitrator.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from Behavior import Behavior
 
 class Arbitrator:
 
+    def __init__(self):
+        pass
+
+    def choose_action(self, behaviors):
+        winning_behavior = None
+        max_weight = -1
+
+        # Skal velge en "vinner behavior" og sende dens motor-recommendation til BBCON
+        for behavior in behaviors:
+            if behavior.weight > max_weight:
+                max_weight = behavior.weight
+                winning_behavior = behavior
+
+        return winning_behavior.motor_recommendations, winning_behavior.halt_request
