6f1c25dcc907ec9212d7baaef3c12259db9f6b3a
Jesper Brynildsen <jesper_bry@hotmail.com>
2017-11-09 09:42:22 +0100

    skrevet update(), get_value(), reset()

--- Sensob.py
+++ Sensob.py
@@ -2,4 +2,16 @@
 
 class Sensob:
 
-    
+    def __init__(self):
+        self.sensors = []
+        self.value = None
+
+    def update(self):
+        return
+
+    def get_value(self):
+        return self.value
+
+    def reset(self):
+        for sensor in self.sensors:
+            sensor.reset()
